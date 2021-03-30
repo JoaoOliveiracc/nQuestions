4113f2d9a410341062711936962b99366a1f9b64
João Monteiro <joao.monteiro@hiperferro.com.br>
2021-03-30 14:05:18 -0300

    Conditional structures with ejs

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ app.set("view engine", "ejs");
 app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
+    var exibirMsg = true;
 
     res.render("index", {
         nome: nome,
-        lang: lang 
+        lang: lang,
+        msg: exibirMsg
     });
 });
 
