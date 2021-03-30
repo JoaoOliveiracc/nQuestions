8231641c0203c6080711f0e6f79d2c3e6d97de69
João Monteiro <joao.monteiro@hiperferro.com.br>
2021-03-30 15:25:03 -0300

    Repetition structures with ejs

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
     var exibirMsg = true;
+    var produtos = [
+        {nome: "Doritos", preco: 3.34},
+        {nome: "Coca-cola", preco: 5},
+        {nome: "Leite", preco: 1.45},
+        {nome: "Pão", preco: 2},
+        {nome: "Bolacha", preco: 1.45}
+    ];
 
     res.render("index", {
         nome: nome,
         lang: lang,
-        msg: exibirMsg
+        msg: exibirMsg,
+        produtos: produtos
     });
 });
 
