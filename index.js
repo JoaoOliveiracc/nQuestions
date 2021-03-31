b5dd6feb7da8c710278c225e94788e70eb3d9a80
João Monteiro <joao.monteiro@hiperferro.com.br>
2021-03-31 12:39:33 -0300

    Form submission

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-
     res.render("index");
 });
 
 app.get("/pergunta", (req, res) => {
     res.render("perguntas");
-})
+});
+
+app.post("/salvarpergunta", (req, res) => {
+    res.send("Salvo!");
+});
 
 app.listen(3000, () => {
     console.log("Servidor iniciado!");
