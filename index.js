ce9bbc6782b2a40d7e7df689962798a0a9b86a52
João Monteiro <joao.monteiro@hiperferro.com.br>
2021-04-01 09:46:14 -0300

    Receiving form on the backend

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParsrer = require("body-parser");
 
 // Usa o ejs como template engine
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+// Body parser
+app.use(bodyParsrer.urlencoded({extended: false}));
+app.use(bodyParsrer.json());
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -14,7 +19,10 @@ app.get("/pergunta", (req, res) => {
 });
 
 app.post("/salvarpergunta", (req, res) => {
-    res.send("Salvo!");
+    var title = req.body.title;
+    var description = req.body.description;
+
+    res.send("Formulário recebido! Título: " + title + " Descrição: " + description);
 });
 
 app.listen(3000, () => {
